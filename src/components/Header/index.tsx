0c23649cbf699f9a8fc444102490b2c0350a63b2
Caique Bilieri <caiqueg4m3r@gmail.com>
2024-03-24 15:23:13 -0300

    Update Header icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,42 @@
-import { View, Text, StyleSheet, } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Feather, FontAwesome6, Entypo} from '@expo/vector-icons';
 import theme from '../../theme'
 
 export function Header() {
     return (
-        <View style={styles.container} >
+        <SafeAreaView style={styles.container} >
             <Text style={styles.text} >
                 Whatsapp
             </Text>
-        </View>
+            <View style={styles.icons} >
+                <Feather name="camera" size={24} color="white" />
+                <FontAwesome6 name="magnifying-glass" size={24} color="white" />
+                <Entypo name="dots-three-vertical" size={24} color="white" />
+            </View>
+        </SafeAreaView>
     );
 }
 
+
+
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: theme.COLORS.GRAY_600
+        backgroundColor: theme.COLORS.GRAY_600,
+        paddingTop: 45,
+        paddingBottom: 10,
+        paddingStart: 14,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: 'center'
     },
 
     text:{
         fontSize: theme.FONT_SIZE.XG,
-        color: theme.COLORS.WHITE
+        color: theme.COLORS.WHITE,
+        fontWeight: 'bold',
+    },
+
+    icons:{
+        flexDirection: 'row',
     }
 });
